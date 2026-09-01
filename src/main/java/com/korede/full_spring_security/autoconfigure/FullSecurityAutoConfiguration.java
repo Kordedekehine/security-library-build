d2ed5deb5b03396d160f54973a0646b64278d516
Korede Salami <salamkorede345@gmail.com>
2026-09-01 08:03:27 -0700

    autoconfigure: scaffold FullSecurityAutoConfiguration
    Placeholder only - the class is empty and the imports file is not yet
    picked up by Boot. Wired up in a following commit.

---
+++ src/main/java/com/korede/full_spring_security/autoconfigure/FullSecurityAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.korede.full_spring_security.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FullSecurityAutoConfiguration {
+
+
+}
